91294e34813eb09488110488ba57c1af4acc5d35
Daniil <daniil@pas>
2021-02-23 19:10:34 +0300

    UI logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,71 @@
+//App vars
+let app;
+
+//UI vars
+let btnRandomize;
+let btnUndoSorting;
+let btnBubbleSort;
+let btnQuickSort;
+let btnStop;
+
+//Entry point
 window.onload = () => {
+    //Init UI
+    btnRandomize = document.getElementById("btnRandomize");
+    btnUndoSorting = document.getElementById("btnUndoSorting");
+    btnBubbleSort = document.getElementById("btnBubbleSort");
+    btnQuickSort = document.getElementById("btnQuickSort");
+    btnStop = document.getElementById("btnStop");
+
+    // Init app
     let containerNode = document.getElementById("array-container");
-    this.App = new App(containerNode);
-    this.App.randomize();
+    app = new App(containerNode);
+    app.randomize();
+    
 };
 
+//
+//UI events
+//
 async function handleBubbleSortClick() {
-    this.App.bubbleSort();
+    sortingStarts();
+    await app.bubbleSort();
+    sortingEnds();
 }
 
 async function handleQuickSortClick() {
-    this.App.quickSort();
+    sortingStarts();
+    await app.quickSort();
+    sortingEnds();
 }
 
 function handleRandomizeClick() {
-    this.App.randomize();
+    app.randomize();
 }
 
 function handleUndoSortingClick() {
-    this.App.undoSorting();
+    app.undoSorting();
 }
 
 function handleStopClick() {
-    this.App.stopSorting();
+    app.stopSorting();
+}
+
+//
+//UI logic
+//
+sortingStarts = () => {
+    btnRandomize.disabled = true;
+    btnUndoSorting.disabled = true;
+    btnBubbleSort.disabled = true;
+    btnQuickSort.disabled = true;
+    btnStop.disabled = false;
+}
+
+sortingEnds = () => {
+    btnRandomize.disabled = false;
+    btnUndoSorting.disabled = false;
+    btnBubbleSort.disabled = false;
+    btnQuickSort.disabled = false;
+    btnStop.disabled = true;
 }
